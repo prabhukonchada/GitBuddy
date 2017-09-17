70074afff84eca5a77458d98edc95cb9a7247877
Prabhu Konchada <prabhukonchada@gmail.com>
2017-09-17 18:21:27 +0530

    Added menu item and toast on click of menu item

--- app/src/main/java/com/example/android/gitbuddy/MainActivity.java
+++ app/src/main/java/com/example/android/gitbuddy/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.android.gitbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText mSearchBoxEditText;
     TextView mUrlDisplayTextView;
     TextView mSearchResultsTextView;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +27,28 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView = (TextView)findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView)findViewById(R.id.tv_github_search_results_json);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menuItemSelected =item.getItemId();
+
+        if(menuItemSelected == R.id.action_search)
+        {
+            Toast.makeText(this,"Search Menu Selected",Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
+    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+    // TODO (14) Don't forgot to call .show() on your Toast
+    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
 }
