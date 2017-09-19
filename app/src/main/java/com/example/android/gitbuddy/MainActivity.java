ca5fe7ed9d38052c0087ce1d554b57a6584f1109
Prabhu Konchada <prabhukonchada@gmail.com>
2017-09-19 16:34:00 +0530

    Using async task to retrieve in background and publish to the UIThread

--- app/src/main/java/com/example/android/gitbuddy/MainActivity.java
+++ app/src/main/java/com/example/android/gitbuddy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.gitbuddy;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +14,7 @@ import com.example.android.gitbuddy.utilities.NetworkUtils;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = (EditText)findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView)findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView)findViewById(R.id.tv_github_search_results_json);
+
     }
 
     @Override
@@ -42,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
         String gitHubQuery = mSearchBoxEditText.getText().toString();
         URL gitHubSearchUrl = NetworkUtils.buildUrl(gitHubQuery);
         mUrlDisplayTextView.setText(gitHubSearchUrl.toString());
+
+        new GitHubQueryTask().execute(gitHubSearchUrl);
+    }
+
+    class GitHubQueryTask extends AsyncTask<URL,Void,String>
+    {
+        @Override
+        protected String doInBackground(URL... urls) {
+            String response="";
+            try {
+                response = NetworkUtils.getResponseFromHttpUrl(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mSearchResultsTextView.setText(s);
+            super.onPostExecute(s);
+        }
     }
 
     @Override
